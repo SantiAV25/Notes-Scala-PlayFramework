c9c3c21700b1137185cb6143db700dd8573d95bb
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-03-31 02:42:24 -0500

    commit to add Note

--- notes-scala-playframework/app/Models/NoteModel.scala
+++ notes-scala-playframework/app/Models/NoteModel.scala
@@ -2,3 +2,17 @@ package Models
 
 
 case class noteModel(noteName: String, bodyNote: String, userId: Int, noteId: Int)
+
+object noteModelObj{
+  import play.api.data.Forms._
+  import play.api.data.Form
+
+  case class DataNotemodel(noteName: String, bodyNote: String)
+
+  val formNotes = Form(
+    mapping(
+      "noteName" -> nonEmptyText,
+      "bodyNote" -> nonEmptyText
+    )(DataNotemodel.apply)(DataNotemodel.unapply)
+  )
+}
--- notes-scala-playframework/app/controllers/NoteController.scala
+++ notes-scala-playframework/app/controllers/NoteController.scala
@@ -15,6 +15,7 @@ import scala.collection._
 class NoteController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
 
     import Models.noteModel
+    import Models.noteModelObj._
     import DataBaseController._
 
     var notesArr: mutable.ArrayBuffer[noteModel] = mutable.ArrayBuffer.empty[noteModel]
@@ -24,7 +25,7 @@ class NoteController @Inject()(cc: MessagesControllerComponents) extends Message
     case Some(userId) => {
       // User is authenticated, render notes page
       selecData(userId)
-      Ok(views.html.createNote(notesArr.toSeq,userId))
+      Ok(views.html.viewNote(notesArr.toSeq, formNotes,userId))
     }
     case None => {
       // User is not authenticated, redirect to login page
@@ -33,7 +34,55 @@ class NoteController @Inject()(cc: MessagesControllerComponents) extends Message
   }
 }
 
+def createNote = Action { implicit request: MessagesRequest[AnyContent] =>
+    val errorFunction = { formWithErrors: Form[DataNotemodel] =>
+      // This is the bad case, where the form had validation errors.
+      // Let's show the user the form again, with the errors highlighted.
+      // Note how we pass the form with errors to the template.
+      BadRequest(views.html.viewNote(notesArr.toSeq,formWithErrors,request.session.get("userId").toString))
+    }
+
+    val successFunction = { data: DataNotemodel =>
+      // This is the good case, where the form was successfully parsed as a Data object.
+       request.session.get("userId") match {
+    case Some(userId) => {
+      // User is authenticated, render notes page
+      insertNote(data.noteName, data.bodyNote ,userId)
+      Redirect("/notes").flashing("info" -> "NoteAdd Correctly")
+    }
+    case None => {
+      // User is not authenticated, redirect to login page
+      Redirect("/").flashing("info" -> "Please log in.")
+    }
+  }
+    }
+
+    val formValidationResult = formNotes.bindFromRequest()
+    formValidationResult.fold(errorFunction, successFunction)
+  }
+
+   def insertNote(noteName: String, bodyNote: String, userId: String): Unit = {
+    val userIdOpt = userId.headOption
+  database.withConnection { conn =>
+    val statement = conn.prepareStatement(
+      """
+        INSERT INTO public."Notes"(
+	    userid, "Notesid", "nombreNotes", "cuerpoNotes")
+	    VALUES (?, Default, ?, ?);
+      """.stripMargin
+    )
+    statement.setInt(1, userId.toInt)
+    statement.setString(2, noteName)
+    statement.setString(3, bodyNote)
+    statement.executeUpdate()
+    statement.close()
+  }
+}
+
+    
+
      def selecData(userId: String): Unit ={
+        notesArr.clear()
     database.withConnection { conn =>
     val statement = conn.prepareStatement(
       s"""
